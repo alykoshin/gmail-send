0a7e338043674cfb038390407656b8d0cd19bf4e
Alexander <alykoshin@gmail.com>
2015-10-04 00:40:56 +0300

    removed dir from params

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 'use strict';
 
-var fs = require('fs');
 var path = require('path');
 var nodemailer = require('nodemailer');
 
@@ -21,32 +20,32 @@ var GMailSend = (function(user, pass) {
   /**
    *
    * @param {string} toAddress
-   * @param {string} dir
    * @param {string || string[]} fnames
    * @param callback
    */
-  self.send = function(toAddress, dir, fnames, callback) {
+  self.send = function(toAddress, fnames, callback) {
     callback = (typeof callback === 'function') ? callback : function() {};
 
     var from = DEFAULT_FROM + ' ' + '<' + DEFAULT_FROM + '>',
-        to = /*toName   + ' ' +*/ toAddress,
+        to   = /*toName   + ' ' +*/ toAddress,
         subject = 'test subject',
         text    = 'test text';
+
+    // Preparing attachments
 
     if (typeof fnames === 'string') { fnames = [fnames]; }
 
     var attachments = [];
     for (var i=0; i<fnames.length; i++) {
-      // Preparing attachment
-      //var img = fs.readFileSync(path.join(dir,fname));
       var attachment = {
-        filename: fnames[i],
-        //contents: img,
-        path: path.join(dir, fnames[i]),
-        cid: fnames[i]
+        path:     fnames[i],
+        filename: path.basename( fnames[i] ),
+        cid:      path.basename( fnames[i] )
       };
       attachments.push(attachment);
     }
+
+    //
 
     var mailOptions = {
       from:    from,    // sender address
@@ -58,6 +57,8 @@ var GMailSend = (function(user, pass) {
     if (DEFAULT_FROM) { mailOptions.replyTo = DEFAULT_FROM; }
 
     console.log('mailOptions: ', mailOptions);
+
+    // Sending email
 
     smtpTransport.sendMail(mailOptions, function(error, info){
       if (error) {
