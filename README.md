efe03a9abbb6dade911a1820cedae533d68f38c0
Alexander <alykoshin@gmail.com>
2015-10-04 00:31:36 +0300

    attachments may be array

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # gmail-send
 Minimalistic module to send email using GMail 
 
-Basically it's a wrapper around nodemailer package to simplify its use.
+Basically it's a wrapper around `nodemailer` package to simplify its usage.
+
+Not yet intended for public use.
--- index.js
+++ index.js
@@ -22,30 +22,38 @@ var GMailSend = (function(user, pass) {
    *
    * @param {string} toAddress
    * @param {string} dir
-   * @param {string} fname
-   * @param {callback} cb
+   * @param {string || string[]} fnames
+   * @param callback
    */
-  self.send = function(toAddress, dir, fname, cb) {
+  self.send = function(toAddress, dir, fnames, callback) {
+    callback = (typeof callback === 'function') ? callback : function() {};
+
     var from = DEFAULT_FROM + ' ' + '<' + DEFAULT_FROM + '>',
         to = /*toName   + ' ' +*/ toAddress,
         subject = 'test subject',
         text    = 'test text';
 
-    // Preparing attachment
-    //var img = fs.readFileSync(path.join(dir,fname));
-    var attachment = {
-      filename: fname,
-      //contents: img,
-      path: path.join(dir, fname),
-      cid: fname
-    };
+    if (typeof fnames === 'string') { fnames = [fnames]; }
+
+    var attachments = [];
+    for (var i=0; i<fnames.length; i++) {
+      // Preparing attachment
+      //var img = fs.readFileSync(path.join(dir,fname));
+      var attachment = {
+        filename: fnames[i],
+        //contents: img,
+        path: path.join(dir, fnames[i]),
+        cid: fnames[i]
+      };
+      attachments.push(attachment);
+    }
 
     var mailOptions = {
       from:    from,    // sender address
       to:      to,      // comma separated list of receivers
       subject: subject, // Subject line
       text:    text,    // plaintext body
-      attachments: [ attachment ]
+      attachments: attachments
     };
     if (DEFAULT_FROM) { mailOptions.replyTo = DEFAULT_FROM; }
 
@@ -54,11 +62,11 @@ var GMailSend = (function(user, pass) {
     smtpTransport.sendMail(mailOptions, function(error, info){
       if (error) {
         console.log('Error sending message:', error);
-        cb(error);
+        callback(error);
 
       } else {
         console.log("Message sent: " + info.response);
-        cb(false, info.response);
+        callback(false, info.response);
       }
     });
   };
